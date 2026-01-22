ad52c0c404d69905ff7c5f64fb0f3d8445bdc915
Tiago Silva <tiago@example.com>
2026-01-21 23:59:49 -0300

    ajuste do readme

--- tests/k6-load/src/simulations/loadPerformanceOutsera.test.js
+++ tests/k6-load/src/simulations/loadPerformanceOutsera.test.js
@@ -1,22 +1,29 @@
+import { group } from "k6";
 import GetAnything from "../requests/getAnything.request";
-import { group, sleep } from "k6";
+import PostAnything from "../requests/postAntything.request";
 
-export const options = {
+export let options = {
+  // Configuração para simular 500 VUs por 5 minutos
   stages: [
-    { duration: "30s", target: 500 }, // Ramp-up agressivo
-    { duration: "4m", target: 500 }, // Carga constante (Sustentação)
-    { duration: "30s", target: 0 }, // Ramp-down
+    { duration: "1m", target: 500 }, // 1. RAMP-UP: Sobe de 0 a 500 usuários em 1 minuto
+    { duration: "5m", target: 500 }, // 2. PLATEAU: Mantém 500 usuários simultâneos por 5 minutos
+    { duration: "30s", target: 0 },  // 3. RAMP-DOWN: Desce suavemente para 0 em 30 segundos
   ],
   thresholds: {
-    http_req_duration: ["p(95)<500"], // SLA de 500ms
-    http_req_failed: ["rate<0.01"], // Erro < 1%
+    http_req_duration: ["p(95)<2000"], // 95% das requisições devem ser mais rápidas que 2s
+    http_req_failed: ["rate<0.05"],    // Taxa de erro deve ser menor que 5%
   },
 };
 
 export default function () {
-  group("Fluxo Crítico - Desafio Outsera", () => {
-    let request = new GetAnything();
-    request.get();
-    sleep(1);
+  let getParams = new GetAnything();
+  let postParams = new PostAnything();
+
+  group("GET - Consulta Geral", () => {
+    getParams.get();
+  });
+
+  group("POST - Criação de Dados", () => {
+    postParams.post();
   });
 }
