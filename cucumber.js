36a558046df58ef2d777f225536ba04f02ba1d81
Tiago Oliveira Silva <tiago.silva@minu.co>
2026-01-29 11:42:47 -0300

    inclusao do allure report

--- cucumber.js
+++ cucumber.js
@@ -1,17 +1,20 @@
-//Configuração central do Cucumber.js para testes E2E.
-
 module.exports = {
   default: {
-    // Define o caminho para os arquivos de especificação (Gherkin)
     paths: ["tests/e2e/features/**/*.feature"],
-    
-    // Utiliza o ts-node para permitir a execução direta de arquivos TypeScript
     requireModule: ["ts-node/register"],
-    
-    // Mapeia a implementação dos passos (steps) e hooks de suporte
     require: ["tests/e2e/steps/**/*.ts", "tests/e2e/support/**/*.ts"],
     
-    // Define o formato de saída: barra de progresso no console e relatório visual em HTML
-    format: ["progress-bar", "html:cucumber-report.html"]
+    // Configuração de Formatos
+    format: [
+      "progress-bar",                    // Barra no terminal
+      "html:cucumber-report.html",       // Relatório HTML simples
+      "allure-cucumberjs/reporter"       // Allure (sem o caminho aqui!)
+    ],
+    
+    formatOptions: {
+      colorsEnabled: true,
+      resultsDir: "allure-results",      // <--- O caminho correto fica aqui
+      dummyFormat: false                 // Workaround para alguns terminais
+    }
   },
 };
